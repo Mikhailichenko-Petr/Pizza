29f0ffab6122b35b2dd0d1bbfdd9d1e0f2a49f7f
Petr <Petruxamix@mail.ru>
2022-09-18 02:10:21 +0300

    BasketImage

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import CartItem from '../Components/CartItem';
 import { clearCart } from '../redux/actions/cart';
+import emptyCart from '../scss/assets/img/emptyCart.png'
 
 function Cart() {
   const dispatch=useDispatch()
@@ -20,7 +22,8 @@ function Cart() {
   return (
     <div className="content">
         <div className="container container--cart">
-          <div className="cart">
+          {
+            totalCount?           <div className="cart">
             <div className="cart__top">
               <h2 className="content__title"><svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M6.33333 16.3333C7.06971 16.3333 7.66667 15.7364 7.66667 15C7.66667 14.2636 7.06971 13.6667 6.33333 13.6667C5.59695 13.6667 5 14.2636 5 15C5 15.7364 5.59695 16.3333 6.33333 16.3333Z" stroke="white" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round"/>
@@ -69,7 +72,21 @@ function Cart() {
                 </div>
               </div>
             </div>
-          </div>
+          </div> : <div className="cart cart--empty">
+          <h2>
+            Корзина пустая <i>😕</i>
+          </h2>
+          <p>
+            Вероятней всего, вы не заказывали ещё пиццу.
+            <br />
+            Для того, чтобы заказать пиццу, перейди на главную страницу.
+          </p>
+          <img src={emptyCart} alt="Empty cart" />
+          <Link to="/" className="button button--black">
+            <span>Вернуться назад</span>
+          </Link>
+        </div>
+          }
         </div>
       </div>
 
