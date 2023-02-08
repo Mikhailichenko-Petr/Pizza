2fc5355cef378c11bc846a71ebb1156b4b27037c
Petr <Petruxamix@mail.com>
2023-02-09 00:37:43 +0300

    new components ButtonCart

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,3 +3,4 @@ export {default as SortPopup} from "./SortPopup"
 export {default as PizzaBlock } from './PizzaBlock';
 export {default as PizzaLoadingBlock} from "./PizzaBlock/LoadingBlock"
 export {default as CartItem} from "./CartItem"
+export {default as ButtonCart} from "./ButtonCart"
